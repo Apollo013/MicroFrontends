063211eb60fe04ec1046a773d04b6ebbaa197790
@Apollo013 <paul.j.millar@gmail.com>
2023-10-16 12:11:30 +0100

    Consuming Remote Modules (Cart)

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
 import { mount } from "products/ProductsIndex";
-import "cart/CartIndex";
+import { mount as cartMount } from "cart/CartIndex";
 
 console.log("Container!");
 
 mount(document.querySelector("#dev-products-container"));
+cartMount(document.querySelector("#dev-cart-container"));
