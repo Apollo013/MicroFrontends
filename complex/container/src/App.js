51d87a2983cf4f2689bfd5622e9e8834dc3df364
@Apollo013 <paul.j.millar@gmail.com>
2023-10-18 12:56:25 +0100

    Setting initial paths for sub apps

--- complex/container/src/App.js
+++ complex/container/src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
-import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+
+import Progress from "./components/Progress";
+import Header from "./components/Header";
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co",
@@ -14,7 +18,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
